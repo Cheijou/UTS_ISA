68b817151367d233db8b3c6a02a28dfdc6ac30bc
alpinnbtk <ambrosiusalvin20@gmail.com>
2025-04-29 13:18:35 +0700

    Tambah Fitur untuk Admin + Finalisasi

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from utils.encryption import encrypt,verify_password, decrypt
 from utils.pdf_generator import generate_resi_pdf
 from utils.password_sensor import input_password
 from utils.buat_resi import generate_random_resi
-from utils.buat_qr import create_resi_qr, read_qr_and_decrypt
+from utils.buat_qr import create_resi_qr, read_qr_and_decrypt, encrypt_data, decrypt_data
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 import json
@@ -49,158 +49,152 @@ def register():
     db.commit()
     print("Registrasi berhasil.", "\n")
 
+def LacakDetailPengiriman(resi):
+    cursor.execute("SELECT p.*, ap.status_pengiriman, ap.lokasi FROM tPengiriman p JOIN tAktivitasPengiriman ap on p.no_resi = ap.tPengiriman_no_resi WHERE p.no_resi = %s", (resi,), " ORDER BY ap.id DESC LIMIT 1")
+    data_pengiriman = cursor.fetchone()
+
+    print("\n", "=== Berikut merupakan detail pengirimannya ===", "\n")
+
+    print("Barang yang Dikirim : ", decrypt_data(data_pengiriman['barang']))
+    print("Tanggal Pengiriman : ", data_pengiriman['tanggal_pengiriman'])
+    print("Pengirim : ", data_pengiriman['pengirim'])
+    print("Penerima : ", data_pengiriman['penerima'])
+    print("Alamat Pengirim : ", decrypt_data(data_pengiriman['alamat_pengirim']))
+    print("Alamat Tujuan : ", decrypt_data(data_pengiriman['alamat_tujuan']))
+    print("Biaya Pengiriman : ", data_pengiriman['harga_pengiriman'])
+    print("Status Pengiriman : ", data_pengiriman['status_pengiriman'])
+    print("Kurir yang Bertugas : ", data_pengiriman['kurir'])
+    print("Lokasi Barang saat ini : ", data_pengiriman['lokasi'], '\n')
+
+    cetak = input("Cetak sebagai PDF? (ya / tidak) : ")
+
+    if cetak == "ya":
+        data_pengiriman_pdf = {
+            "Barang yang Dikirim ": data_pengiriman['barang'],
+            "Tanggal Pengiriman": data_pengiriman['tanggal_pengiriman'],
+            "Pengirim": data_pengiriman['pengirim'],
+            "Penerima": data_pengiriman['penerima'],
+            "Alamat Pengirim ": data_pengiriman['alamat_pengirim'],
+            "Alamat Tujuan": data_pengiriman['alamat_tujuan'],
+            "Biaya Pengiriman": data_pengiriman['harga_pengiriman'],
+            "Status Pengiriman": data_pengiriman['status_pengiriman'],
+            "Kurir yang Bertugas": data_pengiriman['kurir'],
+            "Lokasi Barang saat ini": data_pengiriman['lokasi']
+        }
+        generate_resi_pdf(data_pengiriman_pdf)
+        print("Data pengiriman berhasil dicetak ke file 'data_pengiriman.pdf'", "\n")
+
 def dashboard(user):
     if user['role'] == 'Pengirim':
-        print("1. Kirim Barang")
-        print("2. Lacak Resi")
-        print("0. Keluar")
-        pilihan = input("Pilih menu: ")
-        print()
-        if pilihan == '1':
-            print("=== Buat Pengiriman ===", "\n")
-            print("Silahkan masukkan data-data terkait pengiriman.")
-            nama_penerima = input("Nama Penerima : ")
-            barang = input("Barang yang Dikirim : ")
-            alamat_pengirim = input("Alamat Pengirim : ")
-            alamat_tujuan = input("Alamat Tujuan : ")
-            jenis = input("Pilih Jenis Pengiriman (express/economy)) : ")
-            if (jenis == "express"):
-                harga_pengiriman = 20000
-            else:
-                harga_pengiriman = 8000
-            tanggal_pengiriman = date.today()
-            tanggal_pengiriman_sql = tanggal_pengiriman.isoformat()
-            
-            cek_resi = True
-            while (cek_resi == True):
-                resi = generate_random_resi()
-                cursor.execute("SELECT no_resi FROM tPengiriman WHERE no_resi = %s", (resi,))
-                result = cursor.fetchone()
-                create_resi_qr(resi)
-                if result is None:
-                    cek_resi = False
-            cursor.execute("INSERT INTO tPengiriman (no_resi, pengirim, penerima, barang, harga_pengiriman, alamat_pengirim, alamat_tujuan, tanggal_pengiriman) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (resi, user['username'], nama_penerima, barang, harga_pengiriman, alamat_pengirim, alamat_tujuan, tanggal_pengiriman_sql))
+        pengirim_lanjut = True
+        while pengirim_lanjut == True:
+            print("1. Kirim Barang")
+            print("2. Lacak Resi")
+            print("0. Keluar")
+            pilihan = input("Pilih menu: ")
             print()
-            print("Pengiriman Anda Tercatat, resi anda -->",resi)
-            print("Resi dapat dicetak dengan opsi Lacak Resi -> Input QR Code")
-            db.commit()
-            
-        elif pilihan == '2':
-            print("=== LACAK RESI ===", "\n")
-            print()
-            print("1. Manual \n2. Input QR")
+            if pilihan == '1':
+                print("=== Buat Pengiriman ===", "\n")
+                print("Silahkan masukkan data-data terkait pengiriman.")
+                nama_penerima = input("Nama Penerima : ")
+                barang = input("Barang yang Dikirim : ")
+                alamat_pengirim = input("Alamat Pengirim : ")
+                alamat_tujuan = input("Alamat Tujuan : ")
+                jenis = input("Pilih Jenis Pengiriman (Express / Economy)) : ")
+                if (jenis.lower() == "express"):
+                    harga_pengiriman = 20000
+                else:
+                    harga_pengiriman = 8000
+                tanggal_pengiriman = date.today()
+                tanggal_pengiriman_sql = tanggal_pengiriman.isoformat()
+                
+                cek_resi = True
+                while (cek_resi == True):
+                    resi = generate_random_resi()
+                    cursor.execute("SELECT no_resi FROM tPengiriman WHERE no_resi = %s", (resi,))
+                    result = cursor.fetchone()
+                    create_resi_qr(resi)
+                    if result is None:
+                        cek_resi = False
 
-            pilih_metode = input("Pilih metode pelacakan : ")
-            
-            if(pilih_metode == "1"):
-                resi = input("Masukkan resi yang ingin anda lacak: ")
-                cursor.execute("SELECT * FROM tPengiriman WHERE no_resi = %s", (resi,))
-                data_pengiriman = cursor.fetchone()
+                cursor.execute("INSERT INTO tPengiriman (no_resi, pengirim, penerima, barang, harga_pengiriman, alamat_pengirim, alamat_tujuan, tanggal_pengiriman) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (resi, user['username'], nama_penerima, encrypt_data(barang), harga_pengiriman, encrypt_data(alamat_pengirim), encrypt_data(alamat_tujuan), tanggal_pengiriman_sql))
+                db.commit()
+                cursor.execute("SELECT * FROM tAktivitasPengiriman ORDER BY id DESC LIMIT 1")
+                data_terakhir = cursor.fetchone()
+                if (data_terakhir is None):
+                    id_terakhir = 0
+                else:
+                    id_terakhir = data_terakhir['id']
+                cursor.execute("INSERT INTO tAktivitasPengiriman(id, status_pengiriman, lokasi, tPengiriman_no_resi) VALUES (%s, %s, %s, %s)", (id_terakhir + 1, "Diproses", "Belum Diserahkan", resi))
+                db.commit()
+                print()
 
-                print("\n", "=== Berikut merupakan detail pengirimannya ===", "\n")
+                print("Pengiriman Anda Tercatat, resi anda -->",resi)
+                print("Resi dapat dicetak dengan opsi Lacak Resi -> Input QR Code", "\n")
+                
+            elif pilihan == '2':
+                print("=== LACAK RESI ===", "\n")
+                print()
+                print("1. Manual \n2. Input QR")
 
-                print("Barang yang Dikirim : ", data_pengiriman['barang'])
-                print("Tanggal Pengiriman : ", data_pengiriman['tanggal_pengiriman'])
-                print("Pengirim : ", data_pengiriman['pengirim'])
-                print("Penerima : ", data_pengiriman['penerima'])
-                print("Alamat Pengirim : ", data_pengiriman['alamat_pengirim'])
-                print("Alamat Tujuan : ", data_pengiriman['alamat_tujuan'])
-                print("Biaya Pengiriman : ", data_pengiriman['harga_pengiriman'])
-                print("Kurir yang Bertugas : ", data_pengiriman['kurir'], "\n")
-            elif(pilih_metode == "2"):
-                print("=== Input QR Code ===\n")
-                root = tk.Tk()
-                root.withdraw()
-                root.update()
-                file_path = askopenfilename(title="Select Image!", filetypes=[("Image Files", ".png;.jpg;*.jpeg")])
-                print("tes")
-                hasil = read_qr_and_decrypt(file_path)
-                data = json.loads(hasil)
+                pilih_metode = input("Pilih metode pelacakan : ")
+                
+                if(pilih_metode == "1"):
+                    resi = input("Masukkan resi yang ingin anda lacak: ")
+                    LacakDetailPengiriman(resi)
 
-                cursor.execute("SELECT * FROM tPengiriman WHERE no_resi = %s", (data['resi'],))
-                data_pengiriman = cursor.fetchone()
+                elif(pilih_metode == "2"):
+                    print("=== Input QR Code ===\n")
+                    root = tk.Tk()
+                    root.withdraw()
+                    root.update()
+                    file_path = askopenfilename(title="Select Image!", filetypes=[("Image Files", ".png;.jpg;*.jpeg")])
+                    hasil = read_qr_and_decrypt(file_path)
+                    data = json.loads(hasil)
 
-                print("\n", "=== Berikut merupakan detail pengirimannya ===", "\n")
-
-                print("Barang yang Dikirim : ", data_pengiriman['barang'])
-                print("Tanggal Pengiriman : ", data_pengiriman['tanggal_pengiriman'])
-                print("Pengirim : ", data_pengiriman['pengirim'])
-                print("Penerima : ", data_pengiriman['penerima'])
-                print("Alamat Pengirim : ", data_pengiriman['alamat_pengirim'])
-                print("Alamat Tujuan : ", data_pengiriman['alamat_tujuan'])
-                print("Biaya Pengiriman : ", data_pengiriman['harga_pengiriman'])
-                print("Kurir yang Bertugas : ", data_pengiriman['kurir'], "\n")
-            cetak = input("Cetak sebagai PDF? (ya / tidak) : ")
-
-            if cetak == "ya":
-                data_pengiriman_pdf = {
-                    "Barang yang Dikirim ": data_pengiriman['barang'],
-                    "Tanggal Pengiriman": data_pengiriman['tanggal_pengiriman'],
-                    "Pengirim": data_pengiriman['pengirim'],
-                    "Penerima": data_pengiriman['penerima'],
-                    "Alamat Pengirim ": data_pengiriman['alamat_pengirim'],
-                    "Alamat Tujuan": data_pengiriman['alamat_tujuan'],
-                    "Biaya Pengiriman": data_pengiriman['harga_pengiriman'],
-                    "Kurir yang Bertugas": data_pengiriman['kurir']
-                }
-                generate_resi_pdf(data_pengiriman_pdf)
-                print("Data pengiriman berhasil dicetak ke file 'data_pengiriman.pdf'", "\n")
+                    LacakDetailPengiriman[data['resi']]
+                else:
+                    print("Pilih 1 di antara dua metode di atas!")
             else:
-                dashboard(user)
-        else:
-            main()
+                main()
     elif user['role'] == 'Admin':
-        print("1. Lihat Pengiriman Terbaru")
-        print("0. Keluar")
-        pilihan = input("Pilih menu: ")
-        print()
-
-        if pilihan == "1":
-            print("=== Semua Daftar Pengiriman  ===", "\n")
-            cursor.execute("SELECT * FROM tPengiriman")
-            all_pengiriman = cursor.fetchall()
-
-            for i in range(0, len(all_pengiriman)):
-                print((i+1), ". Pengiriman ", all_pengiriman[i]['barang'], " kepada ", all_pengiriman[i]['penerima'])
+        admin_lanjut = True
+        while admin_lanjut == True:
+            print("1. Lihat Pengiriman Terbaru")
+            print("2. Hapus Data Pengiriman")
+            print("0. Keluar")
+            pilihan = input("Pilih menu: ")
             print()
-            
-            pilihan = int(input("Data pengiriman mana yang ingin anda periksa? : "))
-            id_pengiriman = pilihan
 
-            cursor.execute("SELECT * FROM tPengiriman WHERE no_resi = %s", (all_pengiriman[id_pengiriman-1]["no_resi"],))
-            data_pengiriman = cursor.fetchone()
+            if pilihan == "1":
+                print("=== Semua Daftar Pengiriman  ===", "\n")
+                cursor.execute("SELECT * FROM tPengiriman")
+                all_pengiriman = cursor.fetchall()
 
-            print("\n", "=== Berikut merupakan detail pengirimannya ===", "\n")
+                for i in range(0, len(all_pengiriman)):
+                    print((i+1), ". Pengiriman", all_pengiriman[i]['barang'], "kepada", all_pengiriman[i]['penerima'])
+                print()
+            elif pilihan == "2":
+                print("=== Semua Daftar Pengiriman  ===", "\n")
+                cursor.execute("SELECT * FROM tPengiriman")
+                all_pengiriman = cursor.fetchall()
 
-            print("Barang yang Dikirim : ", data_pengiriman['barang'])
-            print("Tanggal Pengiriman : ", data_pengiriman['tanggal_pengiriman'])
-            print("Pengirim : ", data_pengiriman['pengirim'])
-            print("Penerima : ", data_pengiriman['penerima'])
-            print("Alamat Pengirim : ", data_pengiriman['alamat_pengirim'])
-            print("Alamat Tujuan : ", data_pengiriman['alamat_tujuan'])
-            print("Biaya Pengiriman : ", data_pengiriman['harga_pengiriman'])
-            print("Kurir yang Bertugas : ", data_pengiriman['kurir'], "\n")
+                for i in range(0, len(all_pengiriman)):
+                    print((i+1), ". Pengiriman", all_pengiriman[i]['barang'], "kepada", all_pengiriman[i]['penerima'])
+                print()
 
-            cetak = input("Cetak sebagai PDF? (ya / tidak) : ")
+                pilihan = int(input("Data pengiriman mana yang ingin anda hapus? : "))
+                id_pengiriman = pilihan
 
-            if cetak == "ya":
-                data_pengiriman_pdf = {
-                    "Barang yang Dikirim ": data_pengiriman['barang'],
-                    "Tanggal Pengiriman": data_pengiriman['tanggal_pengiriman'],
-                    "Pengirim": data_pengiriman['pengirim'],
-                    "Penerima": data_pengiriman['penerima'],
-                    "Alamat Pengirim ": data_pengiriman['alamat_pengirim'],
-                    "Alamat Tujuan": data_pengiriman['alamat_tujuan'],
-                    "Biaya Pengiriman": data_pengiriman['harga_pengiriman'],
-                    "Kurir yang Bertugas": data_pengiriman['kurir']
-                }
-                generate_resi_pdf(data_pengiriman_pdf, "data_pengiriman.pdf")
-                print("Data pengiriman berhasil dicetak ke file 'data_pengiriman.pdf'", "\n")
+                cursor.execute("DELETE FROM tAktivitasPengiriman WHERE tPengiriman_no_resi = %s", (all_pengiriman[id_pengiriman-1]["no_resi"],))
+                db.commit()
+                cursor.execute("DELETE FROM tPengiriman WHERE no_resi = %s", (all_pengiriman[id_pengiriman-1]["no_resi"],))
+                db.commit()
+
+                print("Data pengiriman berhasil dihapus.")
             else:
-                dashboard(user)
-        else:
-            main()
-
+                admin_lanjut = False
+                main()
     else:
         kurir_lanjut = True
         while kurir_lanjut == True:
@@ -213,6 +207,7 @@ def dashboard(user):
             if pilihan == '1':
                 cursor.execute("SELECT * FROM tPengiriman WHERE kurir = %s", (user['username'],), " and tanggal_sampai is NULL")
                 jumlah_belum_selesai= cursor.fetchall()
+                print(jumlah_belum_selesai)
                 cursor.execute("SELECT COUNT(*) FROM tPengiriman")
                 temp = cursor.fetchone()
                 total_pengiriman = temp['COUNT(*)']
@@ -220,8 +215,7 @@ def dashboard(user):
                 temp2 = cursor.fetchone()
                 pengiriman_saya = temp2['COUNT(*)']
 
-
-                if jumlah_belum_selesai is None:
+                if not jumlah_belum_selesai:
                     print("=== Pesanan yang dapat Diambil ===", "\n")
                     cursor.execute("SELECT * FROM tPengiriman WHERE kurir is NULL")
                     pesanan_yang_ada = cursor.fetchall()
@@ -240,6 +234,8 @@ def dashboard(user):
                                     "UPDATE tPengiriman SET kurir = %s WHERE no_resi = %s",
                                     (user['username'], resi_pengiriman))
                     db.commit()
+
+                    print("Pesanan berhasil diambil" + "\n")
                 elif pengiriman_saya == total_pengiriman:
                     print('Belum ada Pesanan Pengiriman.','\n')
                 else:
@@ -252,7 +248,7 @@ def dashboard(user):
                 for i in range(0, len(all_pengiriman)):
                     print((i+1), ". Pengiriman", all_pengiriman[i]['barang'], "kepada", all_pengiriman[i]['penerima'])
                     if (all_pengiriman[i]['tanggal_sampai'] is None):
-                        print("Status Pengiriman: Belum Dikirim")
+                        print("Status Pengiriman: Sedang Diproses")
                     else:
                         print("Status Pengiriman: Selesai Dikirim")
                     print()
@@ -298,7 +294,8 @@ def dashboard(user):
                         else:
                              cursor.execute("INSERT INTO tAktivitasPengiriman (id, status_pengiriman, lokasi, tPengiriman_no_resi) VALUES (%s, %s, %s, %s)", (1, status_pengiriman, lokasi_barang, resi_pengiriman))
                         db.commit()
-                        print("\n", "Data terkait pengiriman ", all_pengiriman[urutan_pengiriman-1]['barang'], " kepada ", all_pengiriman[urutan_pengiriman-1]['penerima'], " telah selesai diperbarui", "\n")
+                        print()
+                        print("Data terkait pengiriman", all_pengiriman[urutan_pengiriman-1]['barang'], "kepada", all_pengiriman[urutan_pengiriman-1]['penerima'], "telah selesai diperbarui", "\n")
             elif pilihan == "0":
                 kurir_lanjut = False
                 main()
--- utils/buat_qr.py
+++ utils/buat_qr.py
@@ -19,14 +19,10 @@ def fix_key(key):
 class AESCipher:
     def __init__(self, key):
         self.key = fix_key(key)
-        print(f"[INIT] Key: {self.key.hex()}")  # Pastikan key 32 bytes untuk AES-256
 
     def encrypt(self, raw):
         cipher = AES.new(self.key, AES.MODE_EAX)
         ciphertext, tag = cipher.encrypt_and_digest(raw.encode('utf-8'))
-        print(f"[ENCRYPT] Nonce: {cipher.nonce.hex()}")
-        print(f"[ENCRYPT] Tag: {tag.hex()}")
-        print(f"[ENCRYPT] Ciphertext (first 64 bytes): {ciphertext[:64].hex()}")
         return base64.b64encode(cipher.nonce + tag + ciphertext).decode('utf-8')
 
     def decrypt(self, data):
@@ -34,14 +30,10 @@ class AESCipher:
             enc = base64.b64decode(data.encode('utf-8'))
             if len(enc) < 32:
                 raise ValueError("Data terlalu pendek untuk didekripsi")
-            print(f"[DECRYPT] Data setelah base64 decode, panjang: {len(enc)} bytes")
 
             nonce = enc[:16]  # Ambil nonce dari data yang telah didecode
             tag = enc[16:32]  # Ambil tag
             ciphertext = enc[32:]  # Ambil ciphertext
-            print(f"[DECRYPT] Nonce: {nonce.hex()}")
-            print(f"[DECRYPT] Tag: {tag.hex()}")
-            print(f"[DECRYPT] Ciphertext (first 64 bytes): {ciphertext[:64].hex()}")
             
             # Dekripsi dengan AES.MODE_EAX
             cipher = AES.new(self.key, AES.MODE_EAX, nonce)
@@ -51,7 +43,7 @@ class AESCipher:
             return None
 
 def create_resi_qr(resi = ""):
-    
+
     data_detail = {
         "resi": resi,
     }
@@ -59,7 +51,6 @@ def create_resi_qr(resi = ""):
     # Encrypt the data
     cipher = AESCipher("rahasia123")
     encrypted_data = cipher.encrypt(json.dumps(data_detail))
-    print(encrypted_data)
 
     # Generate QR Code
     qr = qrcode.QRCode(box_size=10, border=4)
@@ -67,18 +58,15 @@ def create_resi_qr(resi = ""):
     qr.make(fit=True)
 
     img = qr.make_image(fill="black", back_color="white")
-    img.save(f"resi_{resi}.png")
+    img.save(f"resi_pengiriman_{resi}.png")
 
 def read_qr_and_decrypt(path):
     img = Image.open(path)
     decoded_objects = decode(img)
-    print(decoded_objects)
     cipher = AESCipher("rahasia123")
     
     for obj in decoded_objects:
         data = obj.data.decode('utf-8')
-        print (data)  # Ambil isi QR Code
-        # print("Isi QR Code:", data)
         
         # Decrypt QR Code content
         decrypted_data = cipher.decrypt(data)
@@ -86,3 +74,17 @@ def read_qr_and_decrypt(path):
             return decrypted_data
         else:
             print("Failed to decrypt or invalid data.")
+
+def encrypt_data(plaintext):
+    cipher = AESCipher("rahasia123")
+
+    encrypted_text = cipher.encrypt(plaintext)
+
+    return encrypted_text
+
+def decrypt_data(ciphertext):
+    cipher = AESCipher("rahasia123")
+
+    decrypted_text = cipher.decrypt(ciphertext)
+
+    return decrypted_text
